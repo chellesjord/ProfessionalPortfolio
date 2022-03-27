9fbd4d1bc5d338d8e96eb3987c13947595a03b9f
chellesjord <m.kayejordan@gmail.com>
2022-03-27 03:35:13 -0500

    minor update

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,49 +1,49 @@
 import React, { useState } from 'react';
 
 const PhotoList = ({ category }) => {
-  const [photos] = useState([
-    {
-      name: 'Menu Masters',
-      category: 'Portfolio',
-      description:
-        'A Pinterest for only recipes. We always want to try something new but never know what to make. With this site, you will be presented with options that are user generated. You can try recipes, upvote the ones you like, comment on ones you have tried and even submit your own.'
-    },
-    {
-      name: 'Personal Crypto Tracker',
-      category: 'Portfolio',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
-    },
-    {
-      name: 'Run Buddy',
-      category: 'Portfolio',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
-    },
-    {
-      name: 'Work-Day Scheduler',
-      category: 'Portfolio',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
-    }
-  ]);
+    const [photos] = useState([
+        {
+            name: 'Menu Masters',
+            category: 'Portfolio',
+            description:
+                'A Pinterest for only recipes. We always want to try something new but never know what to make. With this site, you will be presented with options that are user generated. You can try recipes, upvote the ones you like, comment on ones you have tried and even submit your own.'
+        },
+        {
+            name: 'Personal Crypto Tracker',
+            category: 'Portfolio',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
+        },
+        {
+            name: 'Run Buddy',
+            category: 'Portfolio',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
+        },
+        {
+            name: 'Work-Day Scheduler',
+            category: 'Portfolio',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
+        }
+    ]);
 
-  const currentPhotos = photos.filter(photo => photo.category === category);
+    const currentPhotos = photos.filter(photo => photo.category === category);
 
-  return (
-    <div>
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../../assets/small/Portfolio/${i}.jpg`).default}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            key={image.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex-row">
+                {currentPhotos.map((image, i) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${i}.jpg`).default}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        key={image.description}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default PhotoList;
