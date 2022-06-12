0f56ca3610f179feeb61ac4cca0b598725b82009
chellesjord <m.kayejordan@gmail.com>
2022-06-11 23:26:15 -0500

    switch contact from component to page. Added email.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {HashRouter, Route, Routes } from 'react-router-dom';
-
-
 import Nav from './components/Nav';
+
 // import About from './components/About';
 // import Projects from './components/Projects';
 // import ContactForm from './components/Contact';
 import Footer from './components/Footer';
 
 import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
 import NoMatch from "./pages/NoMatch";
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<AboutPage/>} />
             <Route exact path="/about" element={<AboutPage/>} />
+            <Route exact path="/contact" element={<ContactPage/>} />
             <Route component={NoMatch} />
           </Routes>
           <Footer/>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import About from '../components/Contact';
+import Contact from '../components/Contact/index.js';
 // import Auth from '../utils/auth';
 // import { useQuery } from '@apollo/client';
 
-const ContantPage = () => {
+const ContactPage = () => {
     return (
         <main>
+            <p>
+            Email me: <a href = "mailto: m.kayejordan@gmail.com">m.kayejordan@gmail.com</a> <br></br>
+            Call me: 210-725-9129 or <br></br>
+            <br></br>
+            </p>
             <Contact />
         </main>
     );
